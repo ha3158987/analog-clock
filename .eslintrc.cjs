22ac4f3785d78049833d639a27142912fed2cf9c
ha3158987 <ha3158987@gmail.com>
2024-02-02 01:14:28 +0900

    feat: set up lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,88 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  overrides: [],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    'react-refresh',
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier',
+  ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@{store, utils, routes, hooks}/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@components/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@{api, types, services}/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@{constants, assets, styles}/**',
+            group: 'unknown',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['@tanstack*'],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
   },
-}
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
